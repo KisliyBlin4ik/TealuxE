b3253cae14e9ad5dcc8decfea1fe9303a8e8faa7
Artem <overl0rdold@gmail.com>
2024-04-03 21:10:50 +0300

    two blocks added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-const menuBtn = document.querySelector('.menu__btn')
+const menuBtn = document.querySelector('.menu__btn');
+const menuCLoseBtn = document.querySelector('.menu__close-btn');
+const menuList = document.querySelector('.menu__list');
+const menuOpenBg = document.querySelector('.menu--open-bg');
+
+menuBtn.addEventListener('click', (event) => {
+  menuList.classList.toggle('menu__list--open');
+  menuOpenBg.classList.toggle('menu__list--open');
+});
+menuCLoseBtn.addEventListener('click', (event) => {
+  menuList.classList.remove('menu__list--open');
+  menuOpenBg.classList.remove('menu__list--open');
+});
+menuOpenBg.addEventListener('click', (event) => {
+  if (event.target === menuOpenBg) {
+    menuList.classList.remove('menu__list--open');
+    menuOpenBg.classList.remove('menu__list--open');
+  }
+});
